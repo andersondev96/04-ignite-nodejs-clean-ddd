4e1b4c1b18f520d953454c348b549af479fa335a
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-11-21 00:18:40 -0300

    Configurando o ESLint

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -1,15 +1,15 @@
-import { UniqueEntityId } from "./unique-entity-id"
+import { UniqueEntityId } from './unique-entity-id'
 
 export class Entity<Props> {
-   private _id: UniqueEntityId
-   protected props: any
-   
-   get id() {
-    return this._id
-   }
+  private _id: UniqueEntityId
+  protected props: any
 
-   protected constructor(props: Props, id?: UniqueEntityId) {
+  get id() {
+    return this._id
+  }
+
+  protected constructor(props: Props, id?: UniqueEntityId) {
     this.props = props
     this._id = id ?? new UniqueEntityId(id)
-   }
+  }
 }
