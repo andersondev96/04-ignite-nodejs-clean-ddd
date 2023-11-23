68c4717a851f7e84eeb340afb54161d3b962d0e7
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-11-22 22:53:50 -0300

    Refatorando as pastas

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,4 +1,4 @@
-import { Answer } from '../entities/answer'
+import { Answer } from '../../entreprise/entities/answer'
 
 export interface AnswersRepository {
   create(answer: Answer): Promise<void>
--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,4 +1,4 @@
-import { Answer } from '../entities/answer'
+import { Answer } from '../../entreprise/entities/answer'
 import { AnswersRepository } from '../repositories/answers-repository'
 import { AnswerQuestionUseCase } from './answer-question'
 
