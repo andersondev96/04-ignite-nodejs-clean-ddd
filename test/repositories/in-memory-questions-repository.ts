7f087e73b85e7bb6bc63ad8c19cca5e836bebba6
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-12-16 14:31:50 -0300

    Aggregates & Watched Lists - Excluindo anexos das perguntas

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,8 +1,15 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
+import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { Question } from '@/domain/forum/enterprise/entities/question'
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
+  public items: Question[] = []
+
+  constructor(
+    private questionAttachmentsRepository: QuestionAttachmentsRepository,
+  ) {}
+
   async findById(id: string) {
     const question = this.items.find((item) => item.id.toString() === id)
 
@@ -12,8 +19,6 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
     return question
   }
-
-  public items: Question[] = []
 
   async findBySlug(slug: string) {
     const question = this.items.find((item) => item.slug.value === slug)
@@ -47,5 +52,9 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     const itemIndex = this.items.findIndex((item) => item.id === question.id)
 
     this.items.splice(itemIndex, 1)
+
+    this.questionAttachmentsRepository.deleteManyByQuestionId(
+      question.id.toString(),
+    )
   }
 }
